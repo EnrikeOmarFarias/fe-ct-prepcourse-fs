78ea5f52c53356d8f482c6e6dcedeb988d751a32
enriqueomarfarias <enrikefarias@gmail.com>
2024-05-10 10:23:18 -0300

    Nueva practica

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,29 +5,29 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = '10'
+const nuevoString = "10";
 
 // Crea una variable de tipo number.
-const nuevoNumero = 10
+const nuevoNumero = 10;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = true
+const nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - 7 === 3
+const nuevaResta = 10 - 7 === 3;
 
 // Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * 4 === 40
+const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático.
 const nuevoModulo = 21 % 5 === 1;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   nuevoString,
-   nuevoNumero,
-   nuevoBoolean,
-   nuevaResta,
-   nuevaMultiplicacion,
-   nuevoModulo,
+  nuevoString,
+  nuevoNumero,
+  nuevoBoolean,
+  nuevaResta,
+  nuevaMultiplicacion,
+  nuevoModulo,
 };
